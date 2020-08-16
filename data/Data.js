3771b3d60c0bae753fcb9710454692ffa00da88d
Vigneshbackend <vignesh.krishnasamy@codingmart.com>
2020-08-16 17:31:17 +0530

    inifinite curosel done

---
+++ data/Data.js
@@ -0,0 +1,17 @@
+export const dummyData =
+        [{
+                title: 'title of No 1', url: 'https://i.ibb.co/hYjK44F/anise-aroma-art-bazaar-277253.jpg',
+                description: "description of no 1",
+                id: 1
+
+        },
+        {
+                title: 'title of no 2', url: 'https://i.ibb.co/JtS24qP/food-inside-bowl-1854037.jpg',
+                description: "description of no 2",
+                id: 2
+        },
+        {
+                title: 'title no 3', url: 'https://i.ibb.co/JxykVBt/flat-lay-photography-of-vegetable-salad-on-plate-1640777.jpg',
+                description: "description of no 3",
+                id: 3
+        }]
